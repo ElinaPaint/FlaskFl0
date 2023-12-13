5f201bdb1bf681436e32b9d926347e224289dadd
Eliana <elianapinto20@gmail.com>
2023-12-13 10:34:58 +0100

    port changed

--- app.py
+++ app.py
@@ -151,4 +151,6 @@ def predict_form():
     
     return render_template("form.html", prediction = "N/A")
 
-app.run(debug=True, port=5000)
+
+if __name__ == "__main__":
+	app.run(debug=True, port=8080)
